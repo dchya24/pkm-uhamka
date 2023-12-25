a5f4a03c2a638eed42209ea8323e480d92d1e8a7
dchya24 <cahyadinar241@gmail.com>
2023-12-25 12:21:38 +0700

    Initialize data dosen module

---
+++ app/Models/DataDosen.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DataDosen extends Model
+{
+    use HasFactory;
+
+    protected $table = "data_dosen";
+
+    protected $fillable = [
+        "nidn", "nama", "fakultas", "prodi", "keterangan"
+    ];
+}
